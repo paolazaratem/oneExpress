4d884cb6b3689434a63a2fda0aeb3b58319ea6b4
Paola Zarate GeekGirl <paolazaratem@gmail.com>
2017-04-04 15:31:30 +0200

    add method rout /yo

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ var express = require('express')
 var app = express()
 
 app.get('/', function(req, res){
-    res.send('hihihihii');
+    res.send('hihihihii!!!!!!!!');
+})
+
+app.get('/yo',function(req, res){
+    res.send('YOOOOOOOOOOO');
 })
 
 var server = app.listen(3000, function() {

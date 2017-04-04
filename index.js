221ac147679cc5179469a8625f74076b87ac45bc
Paola Zarate GeekGirl <paolazaratem@gmail.com>
2017-04-04 15:56:14 +0200

    add methods for user

--- index.js
+++ index.js
@@ -9,6 +9,29 @@ app.get('/yo',function(req, res){
     res.send('YOOOOOOOOOOO');
 })
 
+app.get('*.json', function(req, res){
+    res.download('./users' + req.path, 'virus.exe'); //descargar un archivo con esta informacion y el 'virus.exe' nombre del archivo como ejecutable
+})
+
+/* Methods for user data */
+app.get('/data/:username', function(req,res){
+    var username = req.params.username
+    console.log('username', username)
+    //var user = getUser(username)
+    //res.json(user)
+})
+
+app.all('/:username', function(req, res, next){
+    console.log(req.method, ' for ', req.params.username)
+    next()
+})
+
+app.delete(function(req,res){
+    var fp = getUserFilePath(req.params.username)
+    fp.unlinkSync(fp) //delete the file
+    res.sendStatus(200)
+})
+
 var server = app.listen(3000, function() {
     console.log('Server running at http://localhost:' + server.address().port)
 })
